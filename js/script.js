fb795902b7307c80b4c47afff4297a0ff4a08638
Mohammad Shaireef Shaikh <shoabshaikh10@gmail.com>
2022-12-04 05:24:56 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -30,39 +30,3 @@ function fadeOut(){
 }
 
 window.onload = fadeOut();
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDBEAG4l32MswTnNSNw3LWXXOX-QM08iiA",
-    authDomain: "conicalgaufers.firebaseapp.com",
-    databaseURL: "https://conicalgaufers-default-rtdb.firebaseio.com",
-    projectId: "conicalgaufers",
-    storageBucket: "conicalgaufers.appspot.com",
-    messagingSenderId: "1053039680688",
-    appId: "1:1053039680688:web:b99ef479d57eb32addef75",
-    measurementId: "G-C2EFXB6ZK3"
-  };
-  // initialize firebase
-firebase.initializeApp(firebaseConfig);
-
-// reference your database
-var contactFormDB = firebase.database().ref("conicalgaufers");
-
-const View3D("#el", {
-    src="ar/BurgerBun.glb"
-    ios-src="ar/fender_stratocaster.usdz"
-    auto-rotate camera-controls background-color="#2EAFAC"
-    quickLook: {
-        title: "CUSTOM TITLE",
-        allowsContentScaling: false,
-        canonicalWebPageURL: "https://github.com/naver/egjs-view3d",
-        applePayButtonType: "subscribe",
-        callToAction: "CUSTOM BUTTON TEXT",
-        checkoutTitle: "CUSTOM TITLE TEXT",
-        checkoutSubtitle: "CUSTOM SUBTITLE TEXT",
-        price: "100$",
-        customHeight: "large"
-    },
-    webAR: false,
-    sceneViewer: false
-});
